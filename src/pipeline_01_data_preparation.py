526c244997e0ed6de3d765889d7949685c55d9bd
devnarayank <94194256+devnarayank@users.noreply.github.com>
2024-02-01 16:40:41 +0530

    read config

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -4,6 +4,15 @@ import argparse
 import yaml 
 
 
+def read_parms(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def main(config_path, dataasource):
+    config = read_parms(config_path)
+    print(config)
+
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     default_config_path = os.path.join("config", "params.yaml")
@@ -11,4 +20,4 @@ if __name__=="__main__":
     args.add_argument("--dataasource", default=None)
 
     parsed_args = args.parse_args()
-    print(parsed_args.config, parsed_args.dataasource)
+    main(config_path = parsed_args.config, dataasource=parsed_args.dataasource)
